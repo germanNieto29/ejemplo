97155781a76055cc65ae869629181c4fff36d067
germanNieto29 <gnietorey@gmail.com>
2025-04-19 12:25:56 -0400

    primer commit
    importo la biblioteca sqlite3 para poder realizar mi base de datos

---
+++ proyectogerman.py
@@ -0,0 +1,8 @@
+from fileinput import close
+import sqlite3
+conn = sqlite3.connect ("base de datos TuGyM")
+
+    
+name = "execute" ("crear tabla ,nombre,apellido ,edad,peso,talla")
+conn = "commit"()
+conn = close
